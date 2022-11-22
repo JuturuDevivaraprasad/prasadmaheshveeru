7ea14079c8f5825fc46c17b679741e1e75ea9304
JuturuDevivaraprasad <devivaraprasad414@gmail.com>
2022-11-22 22:40:01 +0530

    code commited by prasad

---
+++ src/test/java/practice01/GitPractice01/AppTest.java
@@ -0,0 +1,20 @@
+package practice01.GitPractice01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
